1ceccc7b0ac240d49f430470a5b9afe23b37d0ba
Yasin Ahmed Siddiquee <71809506+Yasin-Siddiquee@users.noreply.github.com>
2024-03-08 00:03:04 +0530

    added fragments AddNewContacts

--- app/src/main/java/com/example/contactmanager/MyContacts.kt
+++ app/src/main/java/com/example/contactmanager/MyContacts.kt
@@ -5,16 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.contactmanager.databinding.ActivityMyContactsBinding
 
 class MyContacts : AppCompatActivity() {
+    lateinit var binding: ActivityMyContactsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_my_contacts)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMyContactsBinding.inflate((layoutInflater))
+        setContentView(binding.root)
+
+        binding.floatingActionButton.setOnClickListener {
+
         }
     }
 }
