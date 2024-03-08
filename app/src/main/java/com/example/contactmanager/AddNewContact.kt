2305fd6ffd8adfd5c50dec684ec0d847d96a8b6d
Yasin Ahmed Siddiquee <71809506+Yasin-Siddiquee@users.noreply.github.com>
2024-03-08 12:13:13 +0530

    replaced AddNewContact Fragment with activity and added contact row

--- app/src/main/java/com/example/contactmanager/AddNewContact.kt
+++ app/src/main/java/com/example/contactmanager/AddNewContact.kt
@@ -1,59 +1,42 @@
 package com.example.contactmanager
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.contactmanager.databinding.ActivityAddNewContactBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class AddNewContact : AppCompatActivity() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AddNewContact.newInstance] factory method to
- * create an instance of this fragment.
- */
-class AddNewContact : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    lateinit var database: DatabaseReference
+    lateinit var binding: ActivityAddNewContactBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        binding = ActivityAddNewContactBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val uniqueID = intent.getStringExtra(MyContacts.KEY)
+
+        binding.btnSave.setOnClickListener {
+            val name = binding.cName.text.toString()
+            val mail = binding.cMail.text.toString()
+            val phone = binding.cNumber.text.toString()
+
+            val contacts = Contacts(name, mail, phone)
+            database = FirebaseDatabase.getInstance().getReference("Users/$uniqueID/Contacts")
+            database.child(name).setValue(contacts).addOnSuccessListener {
+                binding.cName.text?.clear()
+                binding.cMail.text?.clear()
+                binding.cNumber.text?.clear()
+
+                Toast.makeText(this, "New Contact Saved", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+            }
+        }
         }
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_new_contact, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddNewContact.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddNewContact().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
-}
--- app/src/main/java/com/example/contactmanager/MyContacts.kt
+++ app/src/main/java/com/example/contactmanager/MyContacts.kt
@@ -1,5 +1,6 @@
 package com.example.contactmanager
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,13 +9,22 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.contactmanager.databinding.ActivityMyContactsBinding
 
 class MyContacts : AppCompatActivity() {
+    companion object{
+        const val KEY = "com.example.contactmanager.MyContacts.uniqueID"
+    }
     lateinit var binding: ActivityMyContactsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMyContactsBinding.inflate((layoutInflater))
         setContentView(binding.root)
 
+        val uniqueID = intent.getStringExtra(SignIn.KEY)
+
         binding.floatingActionButton.setOnClickListener {
+
+            val intent = Intent(this, AddNewContact::class.java)
+            intent.putExtra(KEY, uniqueID.toString())
+            startActivity(intent)
 
         }
     }
